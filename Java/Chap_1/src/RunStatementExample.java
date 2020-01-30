d3c10cbaae047330bb69e105e671dd4abdb5bbb2
Mingi Kim <mgstyle97.ice@edu.hanbat.ac.kr>
2020-01-30 17:54:54 +0900

    upload java source file

---
+++ Java/Chap_1/src/RunStatementExample.java
@@ -0,0 +1,10 @@
+package Hello;
+
+public class RunStatementExample {
+    public static void main(String[] args){
+        int x = 1;
+        int y = 2;
+        int result = x + y;
+        System.out.println(x + " + " + y + " = " + result);
+    }
+}

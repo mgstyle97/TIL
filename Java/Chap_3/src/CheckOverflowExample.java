b93559f3a46bae01ce136386df920163f248d471
Mingi Kim <mgstyle97.ice@edu.hanbat.ac.kr>
2020-03-10 00:16:42 +0900

    update source directory

---
+++ Java/Chap_3/src/CheckOverflowExample.java
@@ -0,0 +1,24 @@
+public class CheckOverflowExample {
+    public static void main(String[] args) {
+        try {
+            int result = safeAdd(2000000000, 2000000000);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("오버플로우가 발생하여 정확하게 계산할 수 없음");
+        }   // 예외 처리 코드
+    }
+    public static int safeAdd(int left, int right) {
+        if(right > 0) {
+            if(left > (Integer.MAX_VALUE - right)) {
+                throw new ArithmeticException("오버플로우 발생");  // 예외 발생 코드
+            }
+        } else {        // right <= 0일 경우
+            if(left < (Integer.MIN_VALUE - right)) {
+                throw new ArithmeticException("오버플로우 발생");  // 예외 발생 코드
+            }
+        }
+        return left + right;
+    }
+}
+
+

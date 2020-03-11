0ad02f094376a1fa9dee0fd213cfbb705e07f01c
Mingi Kim <mgstyle97.ice@edu.hanbat.ac.kr>
2020-03-11 16:39:38 +0900

    upload Excercise directory

---
+++ Java/Chap_3/src/Excercise/Excercise04.java
@@ -0,0 +1,16 @@
+package Excercise;
+
+public class Excercise04 {
+    public static void main(String[] args) {
+        int pencils = 534;
+        int students = 30;
+
+        // 학생 하 명이 가지는 연필 수
+        int pencilPerStudent = (pencils / students);
+        System.out.println(pencilPerStudent);
+
+        // 남은 연필 수
+        int pencilsLeft = (pencils % students);
+        System.out.println(pencilsLeft);
+    }
+}
---
+++ Java/Chap_3/src/Excercise/Excercise06.java
@@ -0,0 +1,11 @@
+package Excercise;
+
+public class Excercise06 {
+    public static void main(String[] args){
+    int lengthTop = 5;
+    int lengthBottom = 10;
+    int height = 7;
+    double area = (double)((lengthBottom + lengthTop) * height) / 0.5;
+    System.out.println(area);
+    }
+}

be4811f4d81427288685a267927935a0b2e396c5
Mingi Kim <mgstyle97.ice@edu.hanbat.ac.kr>
2020-03-11 16:13:55 +0900

    update source directory

---
+++ Java/Chap_3/src/AssignmentOperatorExmaple.java
@@ -0,0 +1,16 @@
+public class AssignmentOperatorExmaple {
+    public static void main(String[] args) {
+        int result = 0;
+        result += 10;
+        System.out.println("result = " + result);
+        result -= 5;
+        System.out.println("result = " + result);
+        result *= 3;
+        System.out.println("result = " + result);
+        result /= 5;
+        System.out.println("result = " + result);
+        result %= 3;
+        System.out.println("result = " + result);
+    }
+}
+
